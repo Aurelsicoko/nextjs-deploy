bc2f1a9bfe070d0ae0948e569f9fca886f03f4f8
Aurélien Georget <aurelsicoko@gmail.com>
2022-10-06 16:38:28 +0700

    Update layout

--- components/layout.js
+++ components/layout.js
@@ -4,8 +4,8 @@ import styles from './layout.module.css'
 import utilStyles from '../styles/utils.module.css'
 import Link from 'next/link'
 
-const name = '[Your Name]'
-export const siteTitle = 'Next.js Sample Website'
+const name = 'Aurélien'
+export const siteTitle = 'Next.js Website by Aurélien'
 
 export default function Layout({ children, home }) {
   return (
